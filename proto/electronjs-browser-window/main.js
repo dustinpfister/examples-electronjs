aec444d7c768bdea7998c430411f7111fad35af1
dustinpfister <dustin.pfister@gmail.com>
2022-02-14 15:21:18 -0500

    using setMenu method to get more than one menu working

--- proto/electronjs-browser-window/main.js
+++ proto/electronjs-browser-window/main.js
@@ -1,72 +1,109 @@
 // load app and BrowserWindow
-const { app, Menu, BrowserWindow } = require('electron')
+const {
+    app,
+    Menu,
+    BrowserWindow
+} = require('electron')
 
-let mainWin;
+    let mainWin;
 
-function createChildWindow (parentWindow) {
+function createChildWindow(parentWindow) {
+
+
     const childWindow = new BrowserWindow({
-        parent: parentWindow, width: 320, height: 240
-    });
+            parent: parentWindow,
+            width: 320,
+            height: 240
+        });
+
+    const menu = Menu.buildFromTemplate(ChildMenuTemplate);
+    childWindow.setMenu(menu);
+	
     childWindow.loadFile('index.html');
+	
+	
+	
     return childWindow;
 };
 
 // Create the Main browser window.
-function createMainWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    backgroundColor: '#008888',
-    webPreferences: {}
-  })
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-  // Open the DevTools for debugging
-  //mainWindow.webContents.openDevTools()
-  
-  createChildWindow(mainWindow);
-  
-  const menu = Menu.buildFromTemplate(MainMenuTemplate);
-  Menu.setApplicationMenu(menu);
-  
-  return mainWindow;
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            backgroundColor: '#008888',
+            webPreferences: {}
+        })
+        // and load the index.html of the app.
+        mainWindow.loadFile('index.html')
+        // Open the DevTools for debugging
+        //mainWindow.webContents.openDevTools()
+
+        createChildWindow(mainWindow);
+
+    const menu = Menu.buildFromTemplate(MainMenuTemplate);
+    mainWindow.setMenu(menu)
+
+
+    return mainWindow;
 };
 
-
-
-// Custom Menu
+// Custom Menus
 const isMac = process.platform === 'darwin'
-const MainMenuTemplate = [
-  // { role: 'fileMenu' }
-  {
-    label: 'File',
-    submenu: [
-      isMac ? { role: 'close' } : { role: 'quit' }
-    ]
-  },
-  // { role: 'viewMenu' }
-  {
-    label: 'View',
-    submenu: [
-      { type: 'separator' },
-      { role: 'togglefullscreen' }
-    ]
-  }
-]
+    // The main menu for the main window
+    const MainMenuTemplate = [{
+            label: 'File',
+            submenu: [
+                isMac ? {
+                    role: 'close'
+                }
+                 : {
+                    role: 'quit'
+                }
+            ]
+        }, {
+            label: 'View',
+            submenu: [{
+                    type: 'separator'
+                }, {
+                    role: 'togglefullscreen'
+                }
+            ]
+        }
+    ];
+// child window
+
+const ChildMenuTemplate = [{
+            label: 'View',
+            submenu: [{
+                    type: 'separator'
+                }, {
+                    role: 'togglefullscreen'
+                }
+            ]
+        }
+    ];
 
 // the 'ready' event
+/*
 app.whenReady().then(() => {
-  mainWin = createMainWindow();
-  //const child = new BrowserWindow({ parent: mainWin });
-  //child.show();
-  //mainWin.show();
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    mainWin = createMainWindow();
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0){
+            createMainWindow()
+        }
+    })
+});
+*/
+
+app.on('ready', function(){
+	
+	createMainWindow();
+	
 });
 
 // the 'window-all-closed' is also a kind of on quit event
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin')
+        app.quit()
 });
-
