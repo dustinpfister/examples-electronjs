377a362f81dcf44a0480c707e4e6a47acbc0717e
dustinpfister <dustin.pfister@gmail.com>
2022-02-18 14:28:10 -0500

    removed old code from client.js

--- proto/electronjs-app-videoground/html/js/client.js
+++ proto/electronjs-app-videoground/html/js/client.js
@@ -3,78 +3,7 @@
     var WRAP_CANVAS = document.querySelector('#wrap_canvas');
 
 
-/*
-    var getBias = function(per){
-        return 1 - Math.abs(per - 0.5) / 0.5;
-    };
-
-    // create camera helper
-    var createCamera = function(opt){
-        opt = opt || {};
-        var width = 640, height = 480,
-        fieldOfView = 40, aspectRatio = width / height,
-        near = 0.1, far = 1000,
-        camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
-        camera.userData.subject = new THREE.Vector3();
-        return camera;
-    };
-
-    var camMoveMethod = {};
-
-    // follow subject1 method
-    camMoveMethod.followSubject1 = function(camera, per){
-        var bias = getBias(per);
-        return {
-            position: new THREE.Vector3(-8, 5, -8 + 16 * bias), 
-            lookAt: camera.userData.subject
-        };
-    };
-
-    // follow subject2 method
-    camMoveMethod.followSubject2 = function(camera, per){
-        var rad = Math.PI * 2 * per,
-        x = Math.cos(rad) * 6,
-        y = -4 + 8 * getBias(per),
-        z = Math.sin(rad) * 6;
-        return {
-            position: new THREE.Vector3(x, y, z), 
-            lookAt: camera.userData.subject
-        };
-    };
-
-    // move camera update helper
-    var moveCamera = function (camera, per, moveFunc) {
-        var camState = moveFunc(camera, per);
-        // set the position and lookAt values with the
-        // data in the returned camState object
-        camera.position.copy(camState.position)
-        camera.lookAt(camState.lookAt);
-    };
-
     // CAMERA
-    var camera = createCamera();
-
-    // SCENE
-    var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(8, 8))
-    // RENDER
-    var renderer = new THREE.WebGLRenderer();
-    // append to wrap canvas
-    WRAP_CANVAS.appendChild(renderer.domElement);
-    renderer.setSize(640, 480);
-
-    // MESH
-    var mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-    mesh.position.set(3, 0, 0);
-    scene.add(mesh);
-
-    camera.userData.subject = mesh.position;
-
-*/
-
-// CAMERA
     var camera = new THREE.PerspectiveCamera(40, 640 / 480, 0.1, 100);
     camera.position.set(10, 10, 10);
     camera.lookAt(0, 0, 0);
@@ -114,21 +43,7 @@
     var update = function(secs){
         var per = Math.round(frame) / frameMax,
         bias = getBias(per);
-/*
-            methodSecs += secs;
-            if(methodSecs >= 5){
-                methodSecs = 0;
-                methodIndex += 1;
-                methodIndex %= Object.keys(camMoveMethod).length;
-            }
-            // calling camera update method
-            methodName = Object.keys(camMoveMethod)[methodIndex];
-            moveCamera(camera, per, camMoveMethod[methodName]);
-            // moving mesh
-            mesh.position.x = -2 + 4 * bias;
-*/
     };
-
 
     var loop = function () {
         var now = new Date(),
