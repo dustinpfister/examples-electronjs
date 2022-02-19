f8d202b00ab58e27d2e02ebb1f5e9ba58117fd0b
dustinpfister <dustin.pfister@gmail.com>
2022-02-19 15:13:21 -0500

    ui-video-code.js file

---
+++ proto/electronjs-app-videoground/html/js/ui-video-code.js
@@ -0,0 +1,16 @@
+(function () {
+
+   new Vue({
+       el: '#wrap_video_code',
+       template: '<div class="wrap_ui">' +
+           '<textarea></textarea>'+
+       '</div>',
+       data: {
+          sm: sm
+       },
+       methods: {
+       }
+   });
+
+}
+    ());
