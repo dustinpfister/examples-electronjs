5960893400423da9c6b7023a070f8742548b059e
dustinpfister <dustin.pfister@gmail.com>
2022-02-21 15:40:18 -0500

    loading video js text into text area

--- proto/electronjs-app-videoground/html/js/ui-video-code.js
+++ proto/electronjs-app-videoground/html/js/ui-video-code.js
@@ -1,12 +1,13 @@
 (function () {
 
-    new Vue({
+    var vm = new Vue({
         el: '#wrap_video_code',
         template: '<div class="wrap_ui">' +
-            '<textarea cols="60" rows="10"></textarea>'+
+            '<textarea v-model="videoJS" cols="60" rows="10"></textarea>'+
         '</div>',
         data: {
-           sm: sm
+           sm: sm,
+           videoJS: '\/\/ Video JavaScript goes here'
         },
         methods: {
             updateVideo : function(){
@@ -19,6 +20,7 @@
     videoAPI.on('menuOpenFile', function(evnt, text, result){
         console.log(evnt, result);
         console.log(text);
+        vm.$data.videoJS = text;
     });
 
     videoAPI.on('menuError', function(evnt, err){
