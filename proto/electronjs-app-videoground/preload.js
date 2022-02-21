5351cbefd39548022276c58a9b9a76be75147d82
dustinpfister <dustin.pfister@gmail.com>
2022-02-21 15:08:37 -0500

    api local in preload.js

--- proto/electronjs-app-videoground/preload.js
+++ proto/electronjs-app-videoground/preload.js
@@ -1,6 +1,12 @@
 // preload with contextIsolation enabled
 const { contextBridge, ipcRenderer} = require('electron');
 const path = require('path');
+const fs = require('fs')
+
+
+let api = {
+    videoFilePath: path.join(__dirname, 'html/js/video-start.js')
+};
 
 const EVENT = {};
 
@@ -11,20 +17,15 @@ EVENT.menuOpenFile = function(callback){
         callback(evnt, result);
 
     });
+};
 
+api.on = function(eventType, callback){
+   EVENT[eventType](callback);
 }
 
 
 // create an api for window objects in web pages
-contextBridge.exposeInMainWorld('videoAPI', {
-    videoFilePath: path.join(__dirname, 'html/js/video-start.js'), 
-    on: function(eventType, callback){
-
-        EVENT[eventType](callback);
-
-    }
-    
-});
+contextBridge.exposeInMainWorld('videoAPI', api);
 
 
 console.log('preload');
