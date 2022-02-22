86a93cf9a6d27c8c7d6f4a5d2a5a9e9d2082c63a
dustinpfister <dustin.pfister@gmail.com>
2022-02-22 15:19:55 -0500

    APP_NAME const in client

--- proto/electronjs-app-text-edit/html/client.js
+++ proto/electronjs-app-text-edit/html/client.js
@@ -1,6 +1,9 @@
 var text_area = document.querySelector('#text_area'),
 status_area = document.querySelector('#wrap_status>span')
 
+var APP_NAME = 'Text Editor';
+
+document.title = APP_NAME;
 
 var getByteSize = function(text){
     return new Blob([text]).size;
@@ -15,7 +18,7 @@ var updateStatus = function(text){
 myAPI.onMenuOpenFile(function(evnt, text, result){
     var filePath = result.filePaths[0];
     // update title
-    document.title = 'Text Editor' + ' - ' + filePath;
+    document.title = APP_NAME + ' - ' + filePath;
     text_area.value = text;
     updateStatus(text);
 });
