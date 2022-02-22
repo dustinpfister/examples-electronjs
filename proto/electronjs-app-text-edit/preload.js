a369ca6aad5ad7945ad4d557cf8032e26320c3ec
dustinpfister <dustin.pfister@gmail.com>
2022-02-22 15:38:55 -0500

    get text method

--- proto/electronjs-app-text-edit/preload.js
+++ proto/electronjs-app-text-edit/preload.js
@@ -2,26 +2,30 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const fs = require('fs');
 
-// create an api for window objects in web pages
-contextBridge.exposeInMainWorld('textAPI', {
+const textAPI = {
     // CLIENT EVENT for open file option in menu
     onMenuOpenFile: function(callback){
         // should get a result object from main.js
         ipcRenderer.on('menu-open-file', function(evnt, result) {
             const filePath = result.filePaths[0];
-            fs.readFile(filePath, 'utf8', (err, text) => {
-                if(err){
-                    // error reading file
-                    console.log(e.message);
-                }else{
-                    callback(evnt, text, result);
-                }
+            textAPI.getText(filePath, (text)=>{
+                callback(evnt, text, result)
             });
         });
     },
     // CLIENT EVENT for save file option in menu
     onMenuSaveFile: function(callback){
         ipcRenderer.on('menu-save-file', callback);
+    },
+    getText: function(filePath, callback){
+        fs.readFile(filePath, 'utf8', (err, text) => {
+            if(err){
+                // error reading file
+                console.log(e.message);
+            }else{
+                callback(text);
+            }
+        });
     },
     // save the given text to the given file path
     saveText: function(text, filePath){
@@ -30,9 +34,13 @@ contextBridge.exposeInMainWorld('textAPI', {
                 // if error writing file
                 console.log(e.message);
             }
-        })
+        });
     }
-});
+};
+
+
+// create an api for window objects in web pages
+contextBridge.exposeInMainWorld('textAPI', textAPI);
 
 
 console.log('preload');
