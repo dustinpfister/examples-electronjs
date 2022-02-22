911e108b91030e66e78073e900db234e2e500c75
dustinpfister <dustin.pfister@gmail.com>
2022-02-22 15:24:31 -0500

    updating status while typing

--- proto/electronjs-app-text-edit/html/client.js
+++ proto/electronjs-app-text-edit/html/client.js
@@ -1,28 +1,32 @@
-var text_area = document.querySelector('#text_area'),
+const text_area = document.querySelector('#text_area'),
 status_area = document.querySelector('#wrap_status>span')
 
-var APP_NAME = 'Text Editor';
+const APP_NAME = 'Text Editor';
 
 document.title = APP_NAME;
 
-var getByteSize = function(text){
+const getByteSize = (text) => {
     return new Blob([text]).size;
 };
 
-var updateStatus = function(text){
-    var statusText = '';
+const updateStatus = (text) => {
+    let statusText = '';
     statusText += 'size: ' + getByteSize(text) + ' bytes';
     status_area.innerText = statusText;
 };
 
-myAPI.onMenuOpenFile(function(evnt, text, result){
-    var filePath = result.filePaths[0];
+myAPI.onMenuOpenFile((evnt, text, result) => {
+    const filePath = result.filePaths[0];
     // update title
     document.title = APP_NAME + ' - ' + filePath;
     text_area.value = text;
     updateStatus(text);
 });
 
-myAPI.onMenuSaveFile(function(evnt, result){
+myAPI.onMenuSaveFile((evnt, result) => {
     myAPI.saveText(text_area.value, result.filePath)
+});
+
+text_area.addEventListener('keyup', (e) => {
+    updateStatus(e.target.value);
 });
