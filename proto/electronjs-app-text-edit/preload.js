b572604a2bf574f9f03e8167a5888fcfbb529f03
dustinpfister <dustin.pfister@gmail.com>
2022-02-22 15:46:30 -0500

    new style for preload code

--- proto/electronjs-app-text-edit/preload.js
+++ proto/electronjs-app-text-edit/preload.js
@@ -4,37 +4,37 @@ const fs = require('fs'),
 promisify = require('util').promisify,
 readFile = promisify(fs.readFile);
 
-const textAPI = {
-    // CLIENT EVENT for open file option in menu
-    onMenuOpenFile: function(callback){
-        // should get a result object from main.js
-        ipcRenderer.on('menu-open-file', function(evnt, result) {
-            const filePath = result.filePaths[0];
-            textAPI.getText(filePath)
-            .then((text) => {
-                callback(evnt, text, result);
-            })
-            .catch((e) => {
-                ipcRenderer.send('menu-error', e);
-            });
+const textAPI = {};
+
+// CLIENT EVENT for open file option in menu
+textAPI.onMenuOpenFile = function(callback){
+    // should get a result object from main.js
+    ipcRenderer.on('menu-open-file', function(evnt, result) {
+        const filePath = result.filePaths[0];
+        textAPI.getText(filePath)
+        .then((text) => {
+            callback(evnt, text, result);
+        })
+        .catch((e) => {
+            ipcRenderer.send('menu-error', e);
         });
-    },
-    // CLIENT EVENT for save file option in menu
-    onMenuSaveFile: function(callback){
-        ipcRenderer.on('menu-save-file', callback);
-    },
-    getText: function(filePath, callback){
-        return readFile(filePath, 'utf8');
-    },
-    // save the given text to the given file path
-    saveText: function(text, filePath){
-        fs.writeFile(filePath, text, 'utf8', (e) => {
-            if(e){
-                // if error writing file
-                console.log(e.message);
-            }
-        });
-    }
+    });
+};
+// CLIENT EVENT for save file option in menu
+textAPI.onMenuSaveFile = function(callback){
+    ipcRenderer.on('menu-save-file', callback);
+};
+textAPI.getText = function(filePath, callback){
+    return readFile(filePath, 'utf8');
+};
+// save the given text to the given file path
+textAPI.saveText = function(text, filePath){
+    fs.writeFile(filePath, text, 'utf8', (e) => {
+        if(e){
+            // if error writing file
+            console.log(e.message);
+        }
+    });
 };
 
 
