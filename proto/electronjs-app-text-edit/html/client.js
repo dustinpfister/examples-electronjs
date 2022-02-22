2332cb0fd1accb6e6baa89786d05f79fb2ac6ffc
dustinpfister <dustin.pfister@gmail.com>
2022-02-22 15:18:15 -0500

    size display

--- proto/electronjs-app-text-edit/html/client.js
+++ proto/electronjs-app-text-edit/html/client.js
@@ -1,12 +1,25 @@
-var con = document.querySelector('#text_console');
+var text_area = document.querySelector('#text_area'),
+status_area = document.querySelector('#wrap_status>span')
+
+
+var getByteSize = function(text){
+    return new Blob([text]).size;
+};
+
+var updateStatus = function(text){
+    var statusText = '';
+    statusText += 'size: ' + getByteSize(text) + ' bytes';
+    status_area.innerText = statusText;
+};
 
 myAPI.onMenuOpenFile(function(evnt, text, result){
     var filePath = result.filePaths[0];
     // update title
     document.title = 'Text Editor' + ' - ' + filePath;
-    con.value = text;
+    text_area.value = text;
+    updateStatus(text);
 });
 
 myAPI.onMenuSaveFile(function(evnt, result){
-    myAPI.saveText(con.value, result.filePath)
+    myAPI.saveText(text_area.value, result.filePath)
 });
