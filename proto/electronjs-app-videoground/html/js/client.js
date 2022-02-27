4b5833f06f24a192b7bdafcf8377745439ffb6f1
dustinpfister <dustin.pfister@gmail.com>
2022-02-27 13:49:44 -0500

    comments

--- proto/electronjs-app-videoground/html/js/client.js
+++ proto/electronjs-app-videoground/html/js/client.js
@@ -2,27 +2,24 @@
 
     var WRAP_CANVAS = document.querySelector('#wrap_canvas');
 
-
-    // CAMERA
+    // ********** **********
+    // SCENE, CAMERA, and RENDERER
+    // ********** **********
+    var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(40, 640 / 480, 0.1, 100);
     camera.position.set(10, 10, 10);
     camera.lookAt(0, 0, 0);
-
-    // SCENE
-    var scene = new THREE.Scene();
-    // RENDER
     var renderer = new THREE.WebGLRenderer(),
     canvas = renderer.domElement;
     // append to wrap canvas
     WRAP_CANVAS.appendChild(canvas);
     renderer.setSize(640, 480);
-
     canvas.style.width = '320px';
     canvas.style.height = '240px';
 
-    // APP LOOP STATE
-
-    // define the sm object
+    // ********** **********
+    // THE STATE MACHINE (sm) object
+    // ********** **********
     var sm = window.sm = {
         canvas: canvas,
         frame: 0,
@@ -34,7 +31,6 @@
         camera: camera,
         loopActive: false
     };
-
     var secs = 0,
     fps_update = 30,   // fps rate to update ( low fps for low CPU use, but choppy video )
     fps_movement = 60, // fps rate to move camera
@@ -42,30 +38,17 @@
     frameMax = 600,
     loopActive = false,
     lt = new Date();
-
+    // get bias value helper
     var getBias = function(per){
         return 1 - Math.abs(per - 0.5) / 0.5;
     };
-
+    // update
     var update = function(){
         sm.per = Math.round(sm.frame) / sm.frameMax;
         sm.bias = getBias(sm.per);
-
-/*
-        var state = {
-            frame: sm.frame,
-            frameMax: sm.frameMax,
-            per: per,
-            bias: bias,
-            scene: scene,
-            camera: camera
-        };
-*/
-
-
         VIDEO.update(sm, scene, camera, sm.per, sm.bias);
     };
-
+    // app loop
     var loop = function () {
         var now = new Date(),
         secs = (now - lt) / 1000;
@@ -82,7 +65,7 @@
             }
         }
     };
-
+    // setup
     sm.setup = function(){
         sm.frame = 0;
         sm.frameFrac = 0;
@@ -91,14 +74,13 @@
         VIDEO.init(scene, camera);
         sm.setFrame();
     };
-
+    // set frame
     sm.setFrame = function(){
         // call update method
         update(secs);
         // render
         renderer.render(scene, camera);
     };
-
     // start loop
     sm.play = function(){
         sm.loopActive = !sm.loopActive;
