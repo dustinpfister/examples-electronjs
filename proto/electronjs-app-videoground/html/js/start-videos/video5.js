56458c7e215a9041bb775654f4b79e8cda7aabc4
dustinpfister <dustin.pfister@gmail.com>
2022-02-27 13:25:37 -0500

    loading a dae file in start video 5

--- proto/electronjs-app-videoground/html/js/start-videos/video5.js
+++ proto/electronjs-app-videoground/html/js/start-videos/video5.js
@@ -10,17 +10,14 @@ VIDEO.daePaths = [
 VIDEO.init = function(scene, camera){
     // GRID HELPER
     scene.add(new THREE.GridHelper(8, 8));
-    // MESH
-    let mesh = scene.userData.mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-    mesh.position.set(0, 0, 0);
-    scene.add(mesh);
+    //scene.add(VIDEO.daeResults[0].scene.children[2])
+    scene.add(VIDEO.daeResults[0].scene)
+
 };
 
 // update method for the video
 VIDEO.update = function(state, scene, camera, per, bias){
-    let mesh = scene.userData.mesh;
-    mesh.position.x = -4 + 8 * state.bias;
+    //let mesh = scene.userData.mesh;
+    //mesh.position.x = -4 + 8 * state.bias;
 };
 
