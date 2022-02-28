229bcea6614627d5dfc28f6141af37aa35e145ff
dustinpfister <dustin.pfister@gmail.com>
2022-02-28 15:10:11 -0500

    started a utils.js file

---
+++ proto/electronjs-example-videoground/html/js/utils.js
@@ -0,0 +1,10 @@
+var utils = {};
+
+// DAE tools
+utils.DAE = {};
+
+// get the first Mesh or Group type object to be found
+// in the given result object
+utils.DAE.getMesh = function(result){
+
+}
