2217f6c172bd2224e908c112e4f7e34e0b6dc38f
dustinpfister <dustin.pfister@gmail.com>
2022-03-01 16:06:31 -0500

    using guy in video6

--- proto/electronjs-example-videoground/start-videos/video6.js
+++ proto/electronjs-example-videoground/start-videos/video6.js
@@ -4,21 +4,20 @@ VIDEO.scripts = [
 
 // init method for the video
 VIDEO.init = function(sm, scene, camera){
-    // static camera
-    camera.position.set(8, 8, 8);
+    // CAMERA
+    camera.position.set(10, 10, 10);
     camera.lookAt(0, 0, 0);
     // GRID HELPER
     scene.add(new THREE.GridHelper(8, 8));
-
-    // MESH
-    var guy1 = new Guy();
+    // GUY
+    var guy1 = scene.userData.guy1 = new Guy();
+    guy1.group.position.set(0,3,0);
     scene.add(guy1.group);
-
-    
 };
 
 // update method for the video
 VIDEO.update = function(sm, scene, camera, per, bias){
-   
+   var guy1 = scene.userData.guy1;
+   guy1.walk(sm.per, 4);
 };
 
