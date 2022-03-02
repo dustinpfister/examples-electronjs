2b14cb9c5721920ed3292e3e6714f38cf06c40b2
dustinpfister <dustin.pfister@gmail.com>
2022-03-02 11:47:12 -0500

    started draw methods

--- proto/electronjs-example-videoground/start-videos/js/guy-canvas.js
+++ proto/electronjs-example-videoground/start-videos/js/guy-canvas.js
@@ -1,5 +1,30 @@
 var GuyCanvas = (function () {
 
+    var createCanvasTexture = (draw, size) => {
+        var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+        canvas.width = size || 64;
+        canvas.height = size || 64;
+        draw(ctx, canvas);
+        return new THREE.CanvasTexture(canvas);
+    };
+
+    // just draw a solid color background
+    var drawBackground = (ctx, canvas, style) => {
+        ctx.fillStyle = style || 'white';
+        ctx.fillRect(-1, -1, canvas.width + 2, canvas.height + 2);
+    };
+
+    // draw methods
+    var drawMethod = {};
+
+    drawMethod.face1 = (ctx, canvas) => {
+        drawBackground(ctx, canvas, 'white');
+        ctx.fillStyle = 'black';
+        ctx.fillRect(16, 16, 8, 8);
+    };
+
+
     var api = {};
 
     api.createTexture = () => {
