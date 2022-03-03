7ba473ee9eede7fcd117d6f63a0a5bf73d403d6a
dustinpfister <dustin.pfister@gmail.com>
2022-03-03 12:28:26 -0500

    swingPer

--- proto/electronjs-example-videoground/start-videos/video7.js
+++ proto/electronjs-example-videoground/start-videos/video7.js
@@ -103,7 +103,12 @@ VIDEO.update = function(sm, scene, camera, per, bias){
    guy1.moveLegs(sm.per, 16);
    //guy1.walk(sm.per, 16);
    //guy1.moveArms(sm.per, 8);
-   guy1.moveArm('arm_left', 0.5, 0.5);
+   var swingPer = sm.per * 8 % 1,
+   swingBias = 1 - Math.abs(0.5 - swingPer) / 0.5,
+   swing = 0.125 - 0.25 * swingBias;
+
+   guy1.moveArm('arm_left', swing, 0.5);
+   guy1.moveArm('arm_right', swing, 0.5);
 
    guy1.moveHead(0.9 + 0.2 * sm.bias);
    guy1.group.position.set(-8 + 16 * sm.per, 4, 10 * sm.bias);
