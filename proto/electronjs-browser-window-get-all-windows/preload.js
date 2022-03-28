1674e77d69b18a80e6aabc7bb164fe2a1c8bf2de
dustinpfister <dustin.pfister@gmail.com>
2022-03-28 12:06:01 -0400

    preload.js file

---
+++ proto/electronjs-browser-window-get-all-windows/preload.js
@@ -0,0 +1,29 @@
+const { contextBridge, ipcRenderer} = require('electron');
+
+
+// the API to be used in client side code
+let oAPI = {};
+
+
+// the events object
+const EVENT = {};
+
+EVENT.windowCreate = function(callback){
+    ipcRenderer.on('windowCreate', function(evnt, windowObj, windows) {
+        callback(evnt, windowObj, windows);
+    });
+};
+
+EVENT.windowClose = function(callback){
+    ipcRenderer.on('windowClose', function(evnt, windowObj, windows) {
+        callback(evnt, windowObj, windows);
+    });
+};
+
+// The main on method to attach events
+API.on = function(eventType, callback){
+   EVENT[eventType](callback);
+};
+
+// create an API for window objects in web pages
+contextBridge.exposeInMainWorld('API', API);
