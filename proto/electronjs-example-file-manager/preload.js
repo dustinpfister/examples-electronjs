169b960676021f351950e2fb82b36a210eb0dbad
dustinpfister <dustin.pfister@gmail.com>
2022-11-02 11:14:22 -0400

    displaying contents

--- proto/electronjs-example-file-manager/preload.js
+++ proto/electronjs-example-file-manager/preload.js
@@ -1,9 +1,17 @@
 // preload with contextIsolation enabled
 const { contextBridge, ipcRenderer } = require('electron');
 const fs = require('fs');
+const path = require('path');
+const promisify = require('util').promisify;
+
+const readdir = promisify(fs.readdir);
 
 // main file manager api
 const fm = {};
+
+fm.readdir = ( uri ) => {
+    return readdir(uri);
+};
 
 // create an api for window objects in web pages
 contextBridge.exposeInMainWorld('fm', fm);
