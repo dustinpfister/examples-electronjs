45738da5de9b0ab9e7b902124440a108c61fb81f
dustinpfister <dustin.pfister@gmail.com>
2022-11-02 11:24:16 -0400

    array of file names and addtional info

--- proto/electronjs-example-file-manager/preload.js
+++ proto/electronjs-example-file-manager/preload.js
@@ -5,12 +5,24 @@ const path = require('path');
 const promisify = require('util').promisify;
 
 const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
 
 // main file manager api
 const fm = {};
 
 fm.readdir = ( uri ) => {
-    return readdir(uri);
+    // read files array
+    return readdir(uri)
+    // create an array of arrays such as [ [folder, true], [file, false] ]
+    .then( (files) => {
+        return Promise.all(files.map( (fileName) => {
+            const uri_item = path.join(uri, fileName);
+            return stat( uri_item )
+            .then((fStat)=>{
+                 return [fileName, fStat.isDirectory(), uri_item];
+             })
+        }));
+    });
 };
 
 // create an api for window objects in web pages
