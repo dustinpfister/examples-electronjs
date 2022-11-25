f10adcd4a7df0eb5eaa2f5687d084127398dbd61
dustinpfister <dustin.pfister@gmail.com>
2022-11-25 13:36:13 -0500

    comments

--- proto/electronjs-example-file-manager/spaces.js
+++ proto/electronjs-example-file-manager/spaces.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const promisify = require('util').promisify;
-
 const readdir = promisify(fs.readdir);
-
+// can not read a path that has spaces in windows
 readdir('C:/Users/Dustin/My Documents')
 .then( ( files) => {
-	
-	console.log('works okay.');
-	console.log(files);
-	
+    console.log('works okay.');
+    console.log(files);
 })
 .catch((e) => {
-	
-	console.log('nope');
-	console.log(e.message);
-	
+    console.log('nope');
+    console.log(e.message);
 });
