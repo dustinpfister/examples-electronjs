e8b122814700b8b25faf222b93d9800b461b2d88
dustinpfister <dustin.pfister@gmail.com>
2023-10-26 15:54:08 -0400

    made some progress

--- proto/electronjs-example-live-synth/lib/live-synth/synth.js
+++ proto/electronjs-example-live-synth/lib/live-synth/synth.js
@@ -1,3 +1,9 @@
+//-------- ----------
+// TRYING TO WORK OUT SOME CONST VALUES FOR THIS
+//-------- ----------
+
+const ap_buffersize = 100000;
+
 // write a single sample to the given buffer
 const writeSample = (buff, a_sample = 0.5, wave_count = 1, amplitude = 0.3) => {
     const a_waves = a_sample * wave_count % 1;
@@ -6,24 +12,66 @@ const writeSample = (buff, a_sample = 0.5, wave_count = 1, amplitude = 0.3) => {
     return buff;
 };
 //-------- ----------
+// WRITE
+//-------- ----------
+const { spawn } = require('node:child_process');
+const args = ['--buffer-size', ap_buffersize, '-F', 0];
+const aplay = spawn('aplay', args, { stdio: 'pipe' } );
+aplay.stdin._writableState.highWaterMark = 32000;
+aplay.stdout._readableState.highWaterMark = 32000;
+
+const write = (data) => {
+    return aplay.stdin.write(data);
+};
+
+aplay.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+//-------- ----------
 // LOOP
 //-------- ----------
 const buff = Buffer.alloc(1);
-const frame_count = 60;
+const frame_count = 30;
 let i_sample = 0;
 let i_frame = 0;
-const count_sample = 8000;
+const count_sample = 7000;
 let to_high = false;
 let last_time = new Date();
-const ms_fast = 990;
-const ms_slow = 5000;
+
+
+aplay.stdin.on('drain', () => {
+  console.log(`drain done`);
+  to_high = false;
+});
+
+
+let bytes = 0;
+let start_time = new Date();
+
 const loop = () => {
 
-    const t = setTimeout(loop, to_high ? ms_slow: ms_fast);
+    const t2 = new Date() - start_time;
+    const secs = t2 / 1000;
+    const bbs = Math.floor( bytes / secs);
+
+    let ms = 0;
+    if(bbs < 8000){
+       ms = 800;
+    }
+    if(bbs >= 8000){
+       ms = 900;
+    }
+    if(bbs >= 16000){
+       ms = 3000;
+    }
+
+    const t = setTimeout(loop, ms);
+
+    //const t = setTimeout(loop, to_high ? ms_slow: ms_fast);
 
     const a_framecount = (i_frame / frame_count);
 
-    let i_sample = 0;
     while( i_sample < count_sample){
 
 
@@ -36,14 +84,22 @@ const loop = () => {
         // write sample to buffer
         const wave_count = Math.floor(75 + 25 * a_wavecount);
         writeSample(buff, a_sample, wave_count, 0.6);
-        to_high = !process.stdout.write(buff);
+        bytes += 1;
+
+        to_high = !write(buff);
         if(to_high){
+            console.log('to high ' + bbs + ' Bytes/sec' )
             break;
         }
         i_sample += 1;
     }
-
+    i_sample %= count_sample;
     i_frame += 1;
+
+    //if(i_frame % 10 === 0){
+        console.log('tick', 'secs=', secs, 'ms=', ms, 'byes/sec=', bbs);
+    //}
+
     i_frame %= frame_count;
 };
 // drain event
